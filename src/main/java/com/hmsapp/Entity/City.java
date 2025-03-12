f84887b4de8a3d0fb7bd4da875da5391fe8b10ad
Varun B <vb621622@gmail.com>
2025-03-12 23:14:41 +0530

    Update City.java

--- src/main/java/com/hmsapp/Entity/City.java
+++ src/main/java/com/hmsapp/Entity/City.java
@@ -13,7 +13,7 @@ public class City {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private Long id;
-    private int varun;
+    private int john;
 
     @Column(name = "name", length = 45)
     private String name;
