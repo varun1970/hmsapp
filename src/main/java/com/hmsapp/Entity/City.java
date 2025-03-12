6c054086f2db26aa211d374c538ef96febb1df22
Varun <vb05122001@gmail.com>
2025-03-12 23:15:07 +0530

    added man

--- src/main/java/com/hmsapp/Entity/City.java
+++ src/main/java/com/hmsapp/Entity/City.java
@@ -13,7 +13,7 @@ public class City {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private Long id;
-    private int varun;
+    private int man;
 
     @Column(name = "name", length = 45)
     private String name;
